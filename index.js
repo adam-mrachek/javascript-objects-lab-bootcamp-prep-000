b5f19fd312c327da4a24f7bf306cf770ed64ee7e
Adam Mrachek <amrachek@outlook.com>
2018-03-22 22:04:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {};
 function updateObjectWithKeyAndValue (object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
+
+fuction destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return object[key] = value;
+}
